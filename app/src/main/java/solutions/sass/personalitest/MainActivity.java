b1af131f8f77c7f8c958aba73c1ba4d787b62a27
Florian Sass <sass005@cougars.csusm.edu>
2018-02-11 12:04:49 +0100

    Added Personality Class

--- app/src/main/java/solutions/sass/personalitest/MainActivity.java
+++ app/src/main/java/solutions/sass/personalitest/MainActivity.java
@@ -2,14 +2,22 @@ package solutions.sass.personalitest;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RatingBar;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity
 {
+    RatingBar itemRating;
+    TextView itemView, questionView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState)
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        itemRating = (RatingBar)findViewById(R.id.item_rating);
+        itemView = (TextView)findViewById(R.id.item_display);
+        questionView = (TextView)findViewById(R.id.question_display);
     }
 }
---
+++ app/src/main/java/solutions/sass/personalitest/calculations/Personality.java
@@ -0,0 +1,17 @@
+package solutions.sass.personalitest.calculations;
+
+/**
+ * Created by Florian on 11-Feb-18.
+ */
+
+public class Personality
+{
+    String name;
+    String description;
+
+    Personality(String name, String description)
+    {
+        this.name = name;
+        this.description = description;
+    }
+}
